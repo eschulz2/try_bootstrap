4f774b2a702e43462a269caf567e356d585c7152
Eric Schulz <eschulz2@gmail.com>
2013-12-10 15:54:07 -0600

    landing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Store::Application.routes.draw do
+  get "welcome/index"
+
   resources :products
 
 
@@ -51,7 +53,7 @@ Store::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'products#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
